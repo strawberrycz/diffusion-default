9c7e7de9b1e1f5e466beb49231b77d646f0b5889
Tomas Stastnik <strawberrycz@gmail.com>
2023-03-24 10:49:27 +0100

    Fix lint errors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
 import swaggerJSDoc from 'swagger-jsdoc';
